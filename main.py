3f29613ba933978a1ec4cab3284f84facae1e78a
Nick Driscoll <nick.a.mavros@gmail.com>
2021-12-09 21:26:26 -0500

    Made prelude and command case-insensitive

--- main.py
+++ main.py
@@ -40,27 +40,40 @@ def print_dicts(ds):
 	for d in ds:
 		print_dict(d)
 
-def voice_info_from_author(author_voice_infos, author):
+def voice_info_from_author(voice_infos, author):
 	voice_info = None
 	if author.voice == None:
 		return None
 
-	for info in author_voice_infos:
+	for info in voice_infos:
 		if author.voice.channel.id == info.voice_client.channel.id:
 			voice_info = info
 			break
 	return voice_info
 
+async def play_audio_url(text_channel, voice_client, link):
+	stream_url = pafy.new(link).getbestaudio().url
+	await text_channel.send("Now playing %s..." % link)
+	audio_source = discord.FFmpegPCMAudio(stream_url)
+	voice_client.play(audio_source)
+
 class VoiceChannelInfo:
 	def __init__(self, voice_client):
 		self.voice_client = voice_client
 		self.song_queue = []
-		self.stream_process = None
 
 play_keywords = ["p", "play"]
 lyrics_keywords = ["l", "lyrics"]
 disconnect_keywords = ["d", "disc", "disconnect"]
 help_keywords = ["?", "h", "help"]
+
+dash_count = 30
+man_message = "%s**D**riscoll's **S**ound **S**treamer%s\n\n" % ('-'*dash_count, '-'*dash_count)
+man_message += "I am a music bot that can stream music from YouTube given a direct URL or search query.\n\n"
+man_message += "\tTo summon me to your channel to play something:\n\t\t\"!dss play|p <url or query>\"\n"
+man_message += "\tTo expel me from the channel you're in:\n\t\t\"!dss d|disc|disconnect\"\n"
+man_message += "\tTo display this very help message:\n\t\t\"!dss ?|h|help\"\n"
+man_message += "\n\tContact <@%s> to report any issues or bugs." % auth_keys["bot_author_id"]
 
 #This is the main class that subclasses Client
 #Each overridden method represents a Discord event we want to respond to
@@ -74,7 +87,7 @@ class DSSClient(discord.Client):
 
 	#Called when someone begins typing a message.
 	async def on_typing(self, channel, user, when):
-		print("%s started typing in \"%s/%s\" at %s" % (user.name, channel.guild.name, channel.name, when))
+		print("%s started typing in \"%s/%s\" at %s" % (user.name, channel.guild.name, channel.name, when.now()))
 
 	#Called when a message is sent in a guild the bot is a member of
 	async def on_message(self, message):
@@ -85,7 +98,7 @@ class DSSClient(discord.Client):
 		prelude = "!dss" #Beginning part of message that allows us to know it's a command for us to interpret
 
 		#Most messages will make this statement false
-		if message.content[0:len(prelude)] == prelude:
+		if message.content[0:len(prelude)].lower() == prelude:
 			author = message.author
 			channel = message.channel
 
@@ -93,7 +106,7 @@ class DSSClient(discord.Client):
 
 			#Check if we already have a voice client in the author's channel
 			#voice_client becomes None if we don't
-			author_voice_info = voice_info_from_author(self.voice_channel_infos, author)
+			voice_info = voice_info_from_author(self.voice_channel_infos, author)
 
 			content = message.content[len(prelude):]
 			res = re.match(r"\s+([^\s]+)\s*(.*)", content)
@@ -103,43 +116,35 @@ class DSSClient(discord.Client):
 				command = res.group(1)
 				args = res.group(2).split(" ")
 
-				if command in play_keywords:
+				if command.lower() in play_keywords:
 					if author.voice == None:
 						await channel.send("<@%s> You must be in a voice channel to summon me." % author.id)
 						return
 
-					if author_voice_info == None:
+					#If we're not in the message author's voice channel
+					if voice_info == None:
 						for guild_channel in await message.guild.fetch_channels():
 							if guild_channel.id == author.voice.channel.id:
 								await channel.send("Joining voice channel \"%s\"..." % guild_channel.name)
-								author_voice_info = VoiceChannelInfo(await guild_channel.connect())
-								await message.guild.change_voice_state(channel=author_voice_info.voice_client.channel, self_mute=False, self_deaf=True)
-								self.voice_channel_infos.append(author_voice_info)
+								voice_info = VoiceChannelInfo(await guild_channel.connect())
+								await message.guild.change_voice_state(channel=voice_info.voice_client.channel, self_mute=False, self_deaf=True)
+								self.voice_channel_infos.append(voice_info)
 								break
 					else:
-						client = author_voice_info.voice_client
+						client = voice_info.voice_client
 						#if client.is_playing():
 
-						author_voice_info.voice_client.stop()
+						voice_info.voice_client.stop()
 
 					#Play something
 					thing_to_play = res.group(2)
 					if re.search(r"^http[s]?://", thing_to_play):
-						stream_url = pafy.new(thing_to_play).getbestaudio().url
-						await channel.send("Now playing %s..." % thing_to_play)
-						audio_source = discord.FFmpegPCMAudio(stream_url)
-						author_voice_info.voice_client.play(audio_source)
+						await play_audio_url(channel, voice_info.voice_client, thing_to_play)
 					else:
 						await channel.send("Searching for \"%s\"..." % thing_to_play)
 						search = VideosSearch(thing_to_play, limit=1)
 						search_result = search.result()["result"][0]
-
-						stream_url = pafy.new(search_result["link"]).getbestaudio().url
-						await channel.send("Now playing %s..." % search_result["link"])
-						audio_source = discord.FFmpegPCMAudio(stream_url)
-						author_voice_info.voice_client.play(audio_source)
-
-					
+						await play_audio_url(channel, voice_info.voice_client, search_result["link"])					
 
 				elif command == "debug":
 					await message.channel.send("Gotcha")
@@ -147,30 +152,23 @@ class DSSClient(discord.Client):
 
 				elif command in disconnect_keywords:
 					if author.voice == None:
-						await channel.send("<@%s> You may not disconnect me when we're not even in the same room!" % author.id)
+						await channel.send("<@%s> You can't disconnect me when we're not in the same room." % author.id)
 						return
 
-					if author_voice_info == None:
-						await channel.send("I'm not currently connected to a voice channel.")
+					if voice_info == None:
+						await channel.send("I'm not even connected to a voice channel.")
 					else:
 						await channel.send("Disconnecting...")
-						author_voice_info.voice_client.stop()
-						await author_voice_info.voice_client.disconnect()
+						voice_info.voice_client.stop()
+						await voice_info.voice_client.disconnect()
 
-						self.voice_channel_infos.remove(author_voice_info)
+						self.voice_channel_infos.remove(voice_info)
 
 				elif command in help_keywords:
-					dash_count = 30
-					man_message = "%s**D**riscoll's **S**ound **S**treamer%s\n\n" % ('-'*dash_count, '-'*dash_count)
-					man_message += "I am a music streaming bot that can stream music from YouTube given a direct URL or search query.\n\n"
-					man_message += "\tTo summon me to your channel to play something:\n\t\t\"!dss play|p <url or query>\"\n"
-					man_message += "\tTo banish me from the channel you're in:\n\t\t\"!dss d|disc|disconnect\"\n"
-					man_message += "\tTo display this very help message:\n\t\t\"!dss ?|h|help\"\n"
-					man_message += "\n\tContact <@%s> to report any issues or bugs." % auth_keys["bot_author_id"]
 					await channel.send(man_message)
-
+					
 				else:
-					await channel.send("Unrecognized command: %s" % command)
+					await channel.send("Unrecognized command: %s\nenter \"!dss ?\" for the manual" % command)
 
 #Entry point
 def main():
